2705f339d5f6a716a3cd114b1825bf30a3ffe48a
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2022-01-07 21:47:23 +0530

    Added more backgrounds

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,15 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { ApiService } from "./services/api.service";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  constructor(private api: ApiService) {}
+
+  ngOnInit(): void {
+    this.api.fetchData();
+  }
+}
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,9 +1,81 @@
 import { Injectable } from "@angular/core";
-import { Apollo } from "apollo-angular";
+import { Apollo, gql } from "apollo-angular";
+
+export type Player = {
+  id: number;
+  name: string;
+  dob: number;
+  occupation: string;
+  address: string;
+  debt: number;
+  atGameNumber: number;
+  isDead: boolean;
+};
+
+export type Worker = {
+  id: number;
+  name: string;
+  dob: number;
+  occupation: string;
+  address: string;
+  duty: "Manager" | "Guard" | "Utility";
+};
+
+export type Game = {
+  uuid: string;
+  gameNo: number;
+  name: string;
+  description: string;
+  hasCovered: boolean;
+};
 
 @Injectable({
   providedIn: "root",
 })
 export class ApiService {
+  players: Player[] = [];
+  workers: Worker[] = [];
+  games: Game[] = [];
+
   constructor(private apollo: Apollo) {}
+
+  fetchData() {
+    this.apollo
+      .watchQuery<any>({
+        query: gql`
+          query {
+            getAllPlayers {
+              id
+              name
+              dob
+              occupation
+              address
+              debt
+              atGameNumber
+              isDead
+            }
+            getAllWorkers {
+              id
+              name
+              dob
+              occupation
+              address
+              duty
+            }
+            getAllGames {
+              uuid
+              gameNo
+              name
+              description
+              hasCovered
+            }
+          }
+        `,
+      })
+      .valueChanges.subscribe(({ data }) => {
+        this.players = data["getAllPlayers"];
+        this.workers = data["getAllWorkers"];
+        this.games = data["getAllGames"];
+      });
+  }
 }
