b68b2406a23a322800660dab14e0a84abbc6aac6
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2022-01-07 23:25:48 +0530

    Added multiple layout system in players

--- src/app/routes/players/players.component.ts
+++ src/app/routes/players/players.component.ts
@@ -1,13 +1,30 @@
 import { Component, OnInit } from "@angular/core";
 import { ApiService } from "src/app/services/api.service";
 
+enum ViewType {
+  List = "list",
+  Tile = "tile",
+  Table = "table",
+}
+
 @Component({
   selector: "app-players",
   templateUrl: "./players.component.html",
   styleUrls: ["./players.component.scss"],
 })
 export class PlayersComponent implements OnInit {
+  viewType: ViewType = ViewType.Tile;
+  viewType_t = ViewType;
+
   constructor(public api: ApiService) {}
 
   ngOnInit(): void {}
+
+  getClasses() {
+    return {
+      list: this.viewType == ViewType.List,
+      tile: this.viewType == ViewType.Tile,
+      table: this.viewType == ViewType.Table,
+    };
+  }
 }
