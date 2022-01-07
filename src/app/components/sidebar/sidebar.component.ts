307de6ebb1d1b8a0e8429d45cc199fc0b9429e96
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2022-01-07 14:07:54 +0530

    Completed sidebar

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { NavigationEnd, Router } from "@angular/router";
+
+type Routes = {
+  name: string;
+  slug: string;
+};
 
 @Component({
-  selector: 'app-sidebar',
-  templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.scss']
+  selector: "app-sidebar",
+  templateUrl: "./sidebar.component.html",
+  styleUrls: ["./sidebar.component.scss"],
 })
 export class SidebarComponent implements OnInit {
+  endpoint: string = "";
 
-  constructor() { }
+  routes: Routes[] = [
+    { name: "Dashboard", slug: "" },
+    { name: "Players", slug: "players" },
+    { name: "Games", slug: "games" },
+    { name: "Prize", slug: "prize" },
+  ];
+
+  constructor(private router: Router) {}
 
   ngOnInit(): void {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        let slugs = event.url.split("/");
+        if (slugs.length == 1) {
+          this.endpoint = "";
+        } else {
+          this.endpoint = slugs[1];
+        }
+      }
+    });
   }
-
 }
