659f52fd5d8540704f2de4119e9f3d81ea8c6e7a
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2022-01-08 13:31:09 +0530

    Minor fixes

--- src/app/routes/games/games.component.ts
+++ src/app/routes/games/games.component.ts
@@ -19,6 +19,7 @@ export class GamesComponent implements OnInit {
         if (!game.hasCovered) {
           if (this.nextGameNo == -1 && game.gameNo == 6) {
             this.nextGameNo = -1;
+            this.api.moveToNextGame();
           } else {
             this.nextGameNo = game.gameNo;
           }
