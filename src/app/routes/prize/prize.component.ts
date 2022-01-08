042305a71b6a72d3f5a868059872b1dd6fcb75c0
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2022-01-08 15:47:44 +0530

    Added no-winner screen

--- src/app/routes/prize/prize.component.ts
+++ src/app/routes/prize/prize.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { DataService } from "src/app/services/data.service";
 
 @Component({
-  selector: 'app-prize',
-  templateUrl: './prize.component.html',
-  styleUrls: ['./prize.component.scss']
+  selector: "app-prize",
+  templateUrl: "./prize.component.html",
+  styleUrls: ["./prize.component.scss"],
 })
 export class PrizeComponent implements OnInit {
+  constructor(public dataService: DataService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from "@angular/core";
 import { BehaviorSubject } from "rxjs";
+import { Player } from "./api.service";
 
 @Injectable({
   providedIn: "root",
 })
 export class DataService {
   doShowDeadPlayers = new BehaviorSubject<boolean>(false);
+  winner = new BehaviorSubject<Player | undefined>(undefined);
 
   constructor() {}
 }
